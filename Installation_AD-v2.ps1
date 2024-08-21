f6f343ee46903074234ba723c7b6e7e8fd674ec7
gtudu47 <fred.meunier78@gmail.com>
2024-08-21 14:24:58 +0200

    update 1
    first update of script

--- Installation_AD-v2.ps1
+++ Installation_AD-v2.ps1
@@ -9,17 +9,42 @@ $domainName = Read-Host "Domain Name"
     #Name NetBois
 $netbiosname = Read-Host "Name netbios"
     #Patch of NT Directory Services (NTDS)
-$NTDSPath = "C:\Windows\NTDS"
+$NTDSPath = Read-Host "Path NTDS or Defaults"
+if ($NTDSPath -eq $null) {
+    $NTDSPath = "C:\Windows\NTDS"
+} else {
+    Write-Output $NTDSPath
+}
     #Patch Log
-$LogPath = "C:\Windows\NTDS"
+$LogPath = Read-Host "C:\Windows\NTDS"
+if ($LogPath -eq $null) {
+    $LogPath = "C:\Windows\NTDS"
+} else {
+    Write-Output $LogPath
+}
     #Sysvol Patch
 $SysvolPath = "C:\Windows\SYSVOL"
+if ($SysvolPath -eq $null) {
+    $SysvolPath = "C:\Windows\SYSVOL"
+} else {
+    Write-Output $SysvolPath
+}
     #Mode du domaine
-$DomainMode = "Default"
+$DomainMode = Read-Host "Domain mode"
+if ($DomainMode -eq $null) {
+    $DomainMode = "Default"
+} else {
+    Write-Output $DomainMode
+}
     #Installation DNS
 $InstallDNS = $true
     #Mode of forest
 $ForestMode = "Default"
+if ($ForestMode -eq $null) {
+    $ForestMode = "Default"
+} else {
+    Write-Output $ForestMode
+}
     #password admin security
 $SafeModeClearPassword = Read-Host "Safe Mode Clear Password"
 $SafeModeAdministratorPassword = ConvertTo-SecureString $SafeModeClearPassword -AsPlaintext -Force
@@ -33,8 +58,8 @@ Add-WindowsFeature -Name DNS -IncludeManagementTools -IncludeAllSubFeature
 Add-WindowsFeature -Name RSAT-AD-Tools -IncludeManagementTools -IncludeAllSubFeature
 
 #creation delegation
-Install-ADDSForest 
--CreateDnsDelegation:$CreateDnsDelegation `
+Install-ADDSForest `
+-CreateDnsDelegation $CreateDnsDelegation `
 -DomainName $DomainName `
 -DatabasePath $NTDSPath `
 -DomainMode $DomainMode `
