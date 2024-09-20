9f437177a6726f9818b435d1d5c79c27286474fe
gtudu47 <fred.meunier78@gmail.com>
2024-09-20 09:49:53 +0200

    Update script_create_UO
    Changelog :
    - New :
    Comment a script
    Facilitate UO creation at domain name level

--- script_additional/script_Create_UO.ps1
+++ script_additional/script_Create_UO.ps1
@@ -5,15 +5,17 @@
 Import-Module ActiveDirectory
 
 #UO
-$RNewUO = Read-Host "do you want to create a new UO (yes or no)"
-if ($RNewUO -eq "yes" -or $RNewUO -eq "Yes" -or $RNewUO -eq "y" -or $RNewUO -eq "Y") {
+$RNewUO = Read-Host "do you want to create a new UO (yes or no)" 
+while ($RNewUO -eq "yes" -or $RNewUO -eq "Yes" -or $RNewUO -eq "y" -or $RNewUO -eq "Y") {
     $RCreateUO = Read-Host "Create a UO root (1) or Create a UO inside UO (2)"
     if ($RCreateUO -eq 1) {
     # Name UO
         $NameUO = Read-Host "Name the UO"
 
     # Domain Name
-        $DomainNameUO = Read-Host "Domain Name (example: DC=example,DC=com)"
+        $DomainName1 = Read-Host "Name Domain Name (example : example)" #Part1
+        $DomainName2 =Read-Host "Forest Domain Name (example : com)"    #Part2
+        $DomainNameUO = "DC=" + $DomainName1 +","+ "DC=" +$DomainName2  #assembly Domain Name
 
     # Create a new Organizational Unit (OU)
         New-ADOrganizationalUnit -Name $NameUO -Path $DomainNameUO
@@ -23,13 +25,15 @@ if ($RNewUO -eq "yes" -or $RNewUO -eq "Yes" -or $RNewUO -eq "y" -or $RNewUO -eq 
         $NameUO = Read-Host "Name the UO"
     
     # Patch UO
-        $ParentOU = read-host "Parent OU"
-        $DomainNameUO = Read-Host "Domain Name (example: DC=example,DC=com)"
-        $PatchOU = "OU=" + $ParentOU +","+ $DomainNameUO 
+        $ParentOU = read-host "Parent OU"                               #Parent UO
+        $DomainName1 = Read-Host "Name Domain Name (example : example)" #Part 1
+        $DomainName2 =Read-Host "Forest Domain Name (example : com)"    #Part 2
+        $DomainNameUO = "DC=" + $DomainName1 +","+ "DC=" +$DomainName2  #assembly Domain Name
+        $PatchOU = "OU=" + $ParentOU +","+ $DomainNameUO                #assmbly Patch OU
             
     # Create a new Organizational Unit (OU) inside another OU
         New-ADOrganizationalUnit -Name $NameUO -Path $PatchOU
-        #Write-Host $NameUO $ParentOU  $DomainNameUO $PatchOU
         pause
     }
+$RNewUO = Read-Host "do you want to create a new UO (yes or no)"
 }
