090a1f915238abfe560c0775a84d3114468669c9
gtudu47 <fred.meunier78@gmail.com>
2024-09-26 15:16:02 +0200

    Update Script_create_Case.ps1
    Changelog:
    - new : possibility for access right (modification script)

--- script_additional/Script_create_Case.ps1
+++ script_additional/Script_create_Case.ps1
@@ -14,19 +14,34 @@ while ($RNewCase -eq "yes" -or $RNewCase -eq "Yes" -or $RNewCase -eq "y" -or $RN
     if (-Not (Test-Path -Path $FinalPatch)) {
         New-Item -Path $FinalPatch -ItemType Directory
     } else {
-        Write-Host "tth case $FinalPatch already exists."
+        Write-Host "Le dossier $FinalPatch existe déjà."
     }
 
-    #atribute a right SMB
-    #$RNewShare = Read-Host "do you want to create a new Share (yes or no)" 
-    #while ($RNewShare -eq "yes" -or $RNewShare -eq "Yes" -or $RNewShare -eq "y" -or $RNewShare -eq "Y") {
+    # Attribuer des droits NTFS en contrôle total
+    icacls $FinalPatch /grant "Administrateurs:F" /T
+    icacls $FinalPatch /grant "UtilisateurSpécifique:F" /T
+
+    # Attribuer des droits SMB
+    $RNewShare = Read-Host "do you want to create a new Share (yes or no)" 
+    while ($RNewShare -eq "yes" -or $RNewShare -eq "Yes" -or $RNewShare -eq "y" -or $RNewShare -eq "Y") {
     
-        # Create a Share SMB
-    #    New-SmbShare -Name $NameCase -Path $Patch -FullAccess "Administrateurs" -ReadAccess "Utilisateurs"
+        # Check if the share already exists
+        if (-Not (Get-SmbShare -Name $NameCase -ErrorAction SilentlyContinue)) {
+            # Create a Share SMB
+            New-SmbShare -Name $NameCase -Path $FinalPatch -FullAccess "Everyone"
 
-        # Attribuer des droits supplémentaires
-    #    Grant-SmbShareAccess -Name $NameCase -AccountName "UtilisateurSpécifique" -AccessRight Full
-    #}
+            # Attribuer des droits supplémentaires
+            try {
+                Grant-SmbShareAccess -Name $NameCase -AccountName "Everyone" -AccessRight Full
+            } catch {
+                Write-Host "Erreur lors de l'attribution des droits : $_"
+            }
+        } else {
+            Write-Host "Le partage $NameCase existe déjà."
+        }
+        
+        $RNewShare = Read-Host "do you want to create a new Share (yes or no)"
+    }
 
-    $RNewCase = Read-Host "do you want to create a new User (yes or no)"
+    $RNewCase = Read-Host "do you want to create a new Case (yes or no)"
 }
